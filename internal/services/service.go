e15075dda72f5ec48afd6a843363e020553f47be
Ivan Pilipenko <pilipenko437@gmail.com>
2024-08-04 11:33:36 +0300

    v 1.1 commit with goroutine http

--- internal/services/service.go
+++ internal/services/service.go
@@ -6,56 +6,110 @@ import (
 	"sync"
 )
 
+// Service provides account operations
 type Service struct {
-	accounts map[string]*account.Account
-	mu       sync.Mutex
+	accounts   map[string]*account.Account
+	mu         sync.Mutex
+	createCh   chan *account.Account
+	depositCh  chan depositRequest
+	withdrawCh chan withdrawRequest
+	balanceCh  chan balanceRequest
 }
 
+// NewService creates a new Service
 func NewService() *Service {
-	return &Service{
-		accounts: make(map[string]*account.Account),
+	s := &Service{
+		accounts:   make(map[string]*account.Account),
+		createCh:   make(chan *account.Account),
+		depositCh:  make(chan depositRequest),
+		withdrawCh: make(chan withdrawRequest),
+		balanceCh:  make(chan balanceRequest),
+	}
+	go s.run()
+	return s
+}
+
+type depositRequest struct {
+	id     string
+	amount float64
+	result chan error
+}
+
+type withdrawRequest struct {
+	id     string
+	amount float64
+	result chan error
+}
+
+type balanceRequest struct {
+	id     string
+	result chan balanceResponse
+}
+
+type balanceResponse struct {
+	balance float64
+	err     error
+}
+
+func (s *Service) run() {
+	for {
+		select {
+		case acc := <-s.createCh:
+			s.mu.Lock()
+			s.accounts[acc.ID] = acc
+			s.mu.Unlock()
+		case req := <-s.depositCh:
+			s.mu.Lock()
+			acc, exists := s.accounts[req.id]
+			s.mu.Unlock()
+			if !exists {
+				req.result <- errors.New("account not found")
+			} else {
+				req.result <- acc.Deposit(req.amount)
+			}
+		case req := <-s.withdrawCh:
+			s.mu.Lock()
+			acc, exists := s.accounts[req.id]
+			s.mu.Unlock()
+			if !exists {
+				req.result <- errors.New("account not found")
+			} else {
+				req.result <- acc.Withdraw(req.amount)
+			}
+		case req := <-s.balanceCh:
+			s.mu.Lock()
+			acc, exists := s.accounts[req.id]
+			s.mu.Unlock()
+			if !exists {
+				req.result <- balanceResponse{0, errors.New("account not found")}
+			} else {
+				req.result <- balanceResponse{acc.GetBalance(), nil}
+			}
+		}
 	}
 }
 
 func (s *Service) CreateAccount() *account.Account {
-	s.mu.Lock()
-	defer s.mu.Unlock()
 	acc := account.NewAccount(generateID())
-	s.accounts[acc.ID] = acc
+	s.createCh <- acc
 	return acc
 }
 
 func (s *Service) Deposit(id string, amount float64) error {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	acc, exists := s.accounts[id]
-	if !exists {
-		return errors.New("account does not exist")
-	} else {
-		acc.Deposit(amount)
-		return nil
-	}
+	result := make(chan error)
+	s.depositCh <- depositRequest{id, amount, result}
+	return <-result
 }
 
 func (s *Service) Withdraw(id string, amount float64) error {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	acc, exists := s.accounts[id]
-	if !exists {
-		return errors.New("account does not exist")
-	} else {
-		acc.Withdraw(amount)
-		return nil
-	}
+	result := make(chan error)
+	s.withdrawCh <- withdrawRequest{id, amount, result}
+	return <-result
 }
 
 func (s *Service) GetBalance(id string) (float64, error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	acc, exists := s.accounts[id]
-	if !exists {
-		return 0, errors.New("account does not exist")
-	} else {
-		return acc.GetBalance(), nil
-	}
+	result := make(chan balanceResponse)
+	s.balanceCh <- balanceRequest{id, result}
+	res := <-result
+	return res.balance, res.err
 }
